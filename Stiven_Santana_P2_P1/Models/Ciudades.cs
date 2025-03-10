43c7f76edf272d63bf9ff7d95cbc8fe11f9d8092
STIVEN JOSE SANTANA CRUZ <STIVEN_SANTANA@ucne.edu.do>
2025-03-10 18:43:44 -0400

    Crea model Ciudades

---
+++ Stiven_Santana_P2_P1/Models/Ciudades.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Stiven_Santana_P2_P1.Models
+{
+    public class Ciudades
+    {
+        [Key]
+
+        [Required(ErrorMessage ="Este campo es requerido")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Este campo es requerido.")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido.")]
+        public decimal Mpnto { get; set; }
+
+    }
+}
