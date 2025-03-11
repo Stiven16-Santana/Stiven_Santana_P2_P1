8c533a5efa8c33eaca30f321fe22a4eb8fcee5bc
STIVEN JOSE SANTANA CRUZ <STIVEN_SANTANA@ucne.edu.do>
2025-03-11 14:35:18 -0400

    Crea Migracion

---
+++ Stiven_Santana_P2_P1/Migrations/20250311181802_AgregandoColumnaMontoACiudades.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stiven_Santana_P2_P1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregandoColumnaMontoACiudades : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Monto",
+                table: "Ciudades",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Monto",
+                table: "Ciudades");
+        }
+    }
+}
