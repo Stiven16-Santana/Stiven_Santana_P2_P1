f193106a245d6938af07ffb13896c1d35d5a7306
STIVEN JOSE SANTANA CRUZ <STIVEN_SANTANA@ucne.edu.do>
2025-03-11 14:36:02 -0400

    Crea el program

--- Stiven_Santana_P2_P1/Program.cs
+++ Stiven_Santana_P2_P1/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Stiven_Santana_P2_P1.Components;
 using Stiven_Santana_P2_P1.DAL;
+using Stiven_Santana_P2_P1.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,25 +9,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-
-//Obtenemos el ConStr para usarlo en el contexto
+// Obtenemos el ConStr para usarlo en el contexto
 var ConStr = builder.Configuration.GetConnectionString("SqlConStr");
 
-//Agregamos el contexto al builder con el ConStr
+// Agregamos el contexto al builder con el ConStr
 builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
-var app = builder.Build();
+
+// Inyección del servicio (MOVIDO antes de builder.Build())
+builder.Services.AddScoped<CiudadesService>();
+
+var app = builder.Build(); 
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
