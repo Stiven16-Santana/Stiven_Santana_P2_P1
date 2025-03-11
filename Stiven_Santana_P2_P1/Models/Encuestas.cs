86db5a832ae5e59822aab9b2e315b1d0a7235103
STIVEN JOSE SANTANA CRUZ <STIVEN_SANTANA@ucne.edu.do>
2025-03-11 14:33:46 -0400

    Crea modelo de Encuestas

---
+++ Stiven_Santana_P2_P1/Models/Encuestas.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Stiven_Santana_P2_P1.Models;
+
+public class Encuestas
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public DateTime Fecha { get; set; } = DateTime.Now; // Valor por defecto
+
+    [Required]
+    [StringLength(100)]
+    public string Asignatura { get; set; }
+
+    [Required]
+    public decimal Monto { get; set; }
+
+    // Relación con Detalles
+    public List<Ciudades> Detalles { get; set; } = new List<Ciudades>();
+}
