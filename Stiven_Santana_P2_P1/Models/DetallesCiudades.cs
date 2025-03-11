ca39e9fade9df9b8c3d409dc48037ee177f2da50
STIVEN JOSE SANTANA CRUZ <STIVEN_SANTANA@ucne.edu.do>
2025-03-11 14:33:14 -0400

    Crea modelo de DetallesCiudades

---
+++ Stiven_Santana_P2_P1/Models/DetallesCiudades.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Stiven_Santana_P2_P1.Models;
+
+public class Ciudades
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Nombre { get; set; } = string.Empty;
+
+    [Required]
+    public decimal Monto { get; set; }
+}
